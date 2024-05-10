f1ab0a517acda66b97cec58b8ab95a7017d43af3
霂或 <144428946+101796@users.noreply.github.com>
2024-05-10 20:09:40 +0800

    Add files via upload

---
+++ c++作业.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+  void print_star();
+  void print_message();
+  print_star();
+  print_message();
+  return 0;
+}
+void print_star()
+{
+  printf("*******");
+}
+
+void print_message()
+{
+  printf("wdf")
+}
